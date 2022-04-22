892749872c4a4b755a4aa64d0565f3cb5a2e57f6
FrumpyGoose180 <mattia2002@gmail.com>
2022-04-22 21:37:19 +0200

    BUG FIX: - Corretto il percorso dell'include "./Ship.c"

--- struct/player/Player.c
+++ struct/player/Player.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "./Ship.c"
+#include "../ship/Ship.c"
 #include "./Player.h"
 
 // Player writePlayground(Player player, char playground[GRID_SIZE][GRID_SIZE]);

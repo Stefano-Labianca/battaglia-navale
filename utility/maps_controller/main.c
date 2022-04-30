78b93c11efc2e6a180715cb3bbe00b997f4ae283
mattiacurri <mattia2002@gmail.com>
2022-05-01 00:32:16 +0200

    Test

--- utility/maps_controller/main.c
+++ utility/maps_controller/main.c
@@ -1,19 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 #include "../../struct/ship/Ship.c"
 
 int main(void)
 {
 	Player p;
-	
+	Player p2;
+	Round match;
 	p = createPlayer(p, 1);
-	p = buildPlayerNavy(p);
-	
 
-	printf("\n\n\n-- RISULTATO FINALE --\n\n\n");
-	showMap(p.playground);
+	p2 = createPlayer(p, 2);
+	p2 = buildPlayerNavy(p2);
+
+	showMap(p2.playground);
+
+	match = setActivePlayer(match, p);
+	match = setPassivePlayer(match, p2);
+
+	printf("\n");
+	printf("MOSSE");
+	printf("\n");
+
+	char c[1];
+	int r;
+	printf("INSERISCI COLONNA: ");
+	scanf("%c", &c);
+	printf("INSERISCI RIGA: ");
+	scanf("%d", &r);
+	int col;
+	col = getIntegerColumn(c[0]);
+	
+	// match = longShot(r, col, match);
+	// char input;
+	// input = ' ';
+	// input = axisChoice();
+	// if (input == 'R') {
+	// 	match = airStrikeRow(match, r);
+	// } else if (input == 'C') {
+	// 	match = airStrikeColumn(match, col);
+	// }
+	match = radar(match, r, col);
+	p2 = getPassivePlayer(match);
+
+	printf("P2 PLAYGROUND: \n");
+	showMap(p2.playground);
+	p = getActivePlayer(match);
+
+	printf("\n\n P HEATMAP: \n");
+	showMap(p.heatMap);
 	
 	system("pause");
 	return 0;

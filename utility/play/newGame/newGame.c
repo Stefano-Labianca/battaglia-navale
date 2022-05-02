cf7c96c6a1747004fb98ea95c007f4288069f6ab
mattiacurri <mattia2002@gmail.com>
2022-05-02 20:31:07 +0200

    UPDATE - Spacing NEW - Parametro numFile

--- utility/play/newGame/newGame.c
+++ utility/play/newGame/newGame.c
@@ -8,7 +8,7 @@
 #include "../../../global/GlobalConstants.h"
 #include "../../maps_controller/MapsController.h"
 
-void newGame()
+void newGame(int numFile)
 {
     Round round;
     int whoPlay;
@@ -17,29 +17,38 @@ void newGame()
     Player player1;
     Player player2;
     char map[TABLE_MAX][TABLE_MAX];
+    int pause;
 
+    turn = 1;
+    whoPlay = 1;
+    pause = 0;
 
-    turn=1;
-    whoPlay=1;
-    round=setGameRound(round,turn);
-    round=setWhoPlay(round,whoPlay);
+    round = setGameRound(round, turn);
+    round = setWhoPlay(round, whoPlay);
+    round = setPause(round, pause);
+
     printf("CREAZIONE DI PLAYER1\n");
-    id=1;
-    player1=createPlayer(player1,id);
-    player1=buildPlayerNavy(player1);
+    id = 1;
+    player1 = createPlayer(player1, id);
+    player1 = buildPlayerNavy(player1);
+
     printf("\nSTAMPA DEL PLAYGROUND\n");
-    getPlayground(player1,map);
+    getPlayground(player1, map);
     showMap(map);
+
+
     printf("CREAZIONE DI PLAYER2\n");
     id = 2;
     player2 = createPlayer(player2, id);
     player2 = buildPlayerNavy(player2);
+
     printf("\nSTAMPA PLAYGROUND\n");
     getPlayground(player2, map);
     showMap(map);
 
-    round=setActivePlayer(round,player1);
-    round=setPassivePlayer(round,player2);
+
+    round = setActivePlayer(round, player1);
+    round = setPassivePlayer(round, player2);
 
     playGame(round);
     return;
